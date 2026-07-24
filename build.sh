1b72a64fe83f25838561bdf6ba9abe2d3f68e22c
grt-tanish <tanishshr1234@gmail.com>
2026-07-24 20:02:53 +0530

    Run dealer import on deploy

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py seed_harit
+python manage.py import_dealers RenSetu_Dealers_Formatted.xlsx
 python manage.py createsuperuser --noinput || true
 python manage.py shell -c "from django.contrib.sites.models import Site; Site.objects.update_or_create(id=1, defaults={'domain': 'rensetu.in', 'name': 'RenSetu'})"
