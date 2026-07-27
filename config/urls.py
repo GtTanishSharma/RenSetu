3ca6ac0581132b3962f9265c55897eed6e8a56c2
grt-tanish <tanishshr1234@gmail.com>
2026-07-27 22:53:03 +0530

    Fix directory/urls.py self-include causing RecursionError

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,12 @@
 from django.contrib import admin
-from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from django.http import FileResponse
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
-from django.views.generic import TemplateView
-from pathlib import Path
+from django.views.generic import RedirectView, TemplateView
 
 from directory.sitemaps import (
     CategorySitemap, CitySitemap, GuideSitemap, StaticSitemap, ToolSitemap,
 )
-
-# Get BASE_DIR
-from django.conf import settings
-BASE_DIR = settings.BASE_DIR
 
 sitemaps = {
     "static": StaticSitemap,
@@ -28,9 +22,7 @@ urlpatterns = [
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path("llms.txt", TemplateView.as_view(template_name="llms.txt", content_type="text/plain")),
     path("site.webmanifest", TemplateView.as_view(template_name="site.webmanifest", content_type="application/manifest+json")),
-    path("favicon.ico", lambda r: FileResponse(
-        open(BASE_DIR / "directory/static/img/favicon.ico", "rb"),
-        content_type="image/x-icon")),
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("img/favicon.ico"), permanent=True)),
     path("", include("directory.urls")),
 ]
 handler404 = "directory.views.custom_404"
